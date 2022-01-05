672808eae3dd6c0965ac970fe3c3407519111dee
debayan.mitra <debayan.mitra@accenture.com>
2022-01-04 21:58:41 -0600

    flask backend

---
+++ NumberPlateSearch/numberplatesearch/api/app.py
@@ -0,0 +1,14 @@
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/welcome')
+def welcome():
+    return ('Welcome to Number Plate Search Application')
+
+
+@app.route('/time')
+def get_current_time():
+    return {'Now its : ': time.time()}
